2785100a1f738af1ce8575e03ff4cb1a4bd63ad2
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-01 15:51:51 +0100

    add person service

--- src/main/java/com/youcode/spring_boot_tutorial/service/PersonService.java
+++ src/main/java/com/youcode/spring_boot_tutorial/service/PersonService.java
@@ -1,2 +1,16 @@
-package com.youcode.spring_boot_tutorial.service;public class PersonService {
+package com.youcode.spring_boot_tutorial.service;
+
+import com.youcode.spring_boot_tutorial.dao.PersonDao;
+import com.youcode.spring_boot_tutorial.model.Person;
+
+public class PersonService {
+    private final PersonDao personDao;
+
+    public PersonService(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+    return personDao.inserPerson(person);
+    }
 }
