5c1c7d1bd03d8ced03d91511f07ad060f7f746f9
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-01 16:14:22 +0100

    update in person controller

--- src/main/java/com/youcode/spring_boot_tutorial/api/PersonController.java
+++ src/main/java/com/youcode/spring_boot_tutorial/api/PersonController.java
@@ -1,4 +1,16 @@
 package com.youcode.spring_boot_tutorial.api;
 
+import com.youcode.spring_boot_tutorial.model.Person;
+import com.youcode.spring_boot_tutorial.service.PersonService;
+
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+    public void addPerson(Person person){
+        personService.addPerson(person);
+    }
 }
