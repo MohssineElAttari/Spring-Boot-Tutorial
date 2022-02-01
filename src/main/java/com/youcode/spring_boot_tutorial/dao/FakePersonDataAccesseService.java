a8617c8737b1023dfa99bdd3b840cc55816c53f1
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-01 15:50:12 +0100

    add fake Person data access

--- src/main/java/com/youcode/spring_boot_tutorial/dao/FakePersonDataAccesseService.java
+++ src/main/java/com/youcode/spring_boot_tutorial/dao/FakePersonDataAccesseService.java
@@ -1,2 +1,16 @@
-package com.youcode.spring_boot_tutorial.dao;public class FakePersonDataAccesseService {
+package com.youcode.spring_boot_tutorial.dao;
+
+import com.youcode.spring_boot_tutorial.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FakePersonDataAccesseService implements  PersonDao{
+    private static List<Person> DB = new ArrayList<>();
+    @Override
+    public int inserPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 0;
+    }
 }
