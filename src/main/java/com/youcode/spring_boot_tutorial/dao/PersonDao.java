b2301c17c40d16fb96fe45ac27ee937f2fe52d5d
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-01 15:50:58 +0100

    add person dao

--- src/main/java/com/youcode/spring_boot_tutorial/dao/PersonDao.java
+++ src/main/java/com/youcode/spring_boot_tutorial/dao/PersonDao.java
@@ -1,2 +1,13 @@
-package com.youcode.spring_boot_tutorial.dao;public interface PersonDao {
+package com.youcode.spring_boot_tutorial.dao;
+
+import com.youcode.spring_boot_tutorial.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+    int inserPerson(UUID id , Person person);
+    default int inserPerson(Person person){
+        UUID id =UUID.randomUUID();
+        return  inserPerson(id,person);
+    }
 }
